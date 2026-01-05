0d46251dd48c0bcca6fa7d10817bd6666902fee8
Arbaaz <arbaazcode@gmail.com>
2026-01-05 21:03:49 +0530

    testing script of Fraction DataType

---
+++ Project-1/data_type_test.py
@@ -0,0 +1,23 @@
+from datatype import Fraction
+
+
+# testcases
+
+x = Fraction(3, 6)
+y = Fraction(4, 8)
+
+print("Fraction 1: ", x)
+print("Fraction 2: ", y)
+
+result = x + y
+print("Addition Result: ", result)
+
+result = x - y
+print("Subtraction Result: ", result)
+
+result = x * y
+print("Multiplication Result: ", result)
+
+result = x / y
+print("Division Result: ", result)
+
